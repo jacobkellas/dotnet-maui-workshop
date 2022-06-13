45240ec4d889da8571a6e26c23200abe303c1114
Jake Kellas <jacob.kellas@sdsheriff.org>
2022-06-13 14:41:03 -0700

    3:56 in

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -3,10 +3,31 @@
 [QueryProperty("Monkey", "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
-    public MonkeyDetailsViewModel()
+    private readonly IMap map;
+
+    public MonkeyDetailsViewModel(IMap map)
     {
+        this.map = map;
     }
 
     [ObservableProperty]
     private Monkey monkey;
+
+    [ICommand]
+    private async Task OpenMapAsync()
+    {
+        try
+        {
+            await map.OpenAsync(Monkey.Latitude, Monkey.Longitude, new MapLaunchOptions
+            {
+                Name = Monkey.Name,
+                NavigationMode = NavigationMode.None
+            });
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", $"Unable to open map: {ex.Message}", "OK");
+        }
+    }
 }
--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -6,11 +6,62 @@ public partial class MonkeysViewModel : BaseViewModel
 {
     private readonly MonkeyService monkeyService;
     public ObservableCollection<Monkey> Monkeys { get; } = new();
+    private readonly IConnectivity connectivity;
+    private readonly IGeolocation geolocation;
 
-    public MonkeysViewModel(MonkeyService monkeyService)
+
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
         Title = "Monkey Finder";
         this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+        this.geolocation = geolocation;
+    }
+
+    [ObservableProperty]
+    private bool isRefreshing;
+
+    [ICommand]
+    private async Task GetClosestMonkeyAsync()
+    {
+        if(IsBusy || Monkeys.Count == 0)
+        {
+            return;
+        }
+
+        try
+        {
+            var location = await geolocation.GetLastKnownLocationAsync();
+            if (location is null)
+            {
+                location = await geolocation.GetLocationAsync(
+                    new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30),
+                    });
+            }
+
+            if (location is null)
+            {
+                return;
+            }
+
+            var first = Monkeys.OrderBy(m => location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Miles)).FirstOrDefault();
+
+            if (first is null)
+            {
+                return;
+            }
+
+            await Shell.Current.DisplayAlert("Closest Monkey",
+                $"{first.Name} in {first.Location}", "OK");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", $"Unable to get closest monkey: {ex.Message}", "OK");
+        }
     }
 
     [ICommand]
@@ -38,6 +89,11 @@ public partial class MonkeysViewModel : BaseViewModel
 
         try
         {
+            if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("Internet Issue", "Check your internet and try again", "OK");
+                return;
+            }
             IsBusy = true;
             var monkeys = await monkeyService.GetMonkeys();
 
@@ -59,6 +115,7 @@ public partial class MonkeysViewModel : BaseViewModel
         finally
         {
             IsBusy = false;
+            IsRefreshing = false;
         }
     }
 }
